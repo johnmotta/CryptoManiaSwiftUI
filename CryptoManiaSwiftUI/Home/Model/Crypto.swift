a01a1ef002868d824b1c26e08a4199c5f6233ccc
John Motta <gabriel_motta31@hotmail.com>
2024-10-23 10:49:16 -0300

    Change name and init

--- CryptoManiaSwiftUI/Home/Model/Crypto.swift
+++ CryptoManiaSwiftUI/Home/Model/Crypto.swift
@@ -8,16 +8,24 @@
 import Foundation
 
 struct ResponseData: Codable {
-    let data: [Cripto]
+    let data: [Crypto]
 }
 
-struct Crypto: Codable {
+struct Crypto: Codable, Identifiable {
     let id: Int
     var favorite: Bool?
     let name: String
     let symbol: String
     let quote: Quote
 
+    init() {
+        self.id = Int()
+        self.favorite = Bool()
+        self.name = String()
+        self.symbol = String()
+        self.quote = Quote(usd: Usd(price: 0, percentChange1h: 0, percentChange24h: 0, percentChange7d: 0))
+    }
+    
     enum CodingKeys: String, CodingKey {
         case id
         case favorite
