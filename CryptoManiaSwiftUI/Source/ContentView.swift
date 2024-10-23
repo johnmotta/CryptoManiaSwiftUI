6588915122f4dbf5922a6ee88aab30d1436f09b5
John Motta <gabriel_motta31@hotmail.com>
2024-10-23 09:00:37 -0300

    Organizing folder

---
+++ CryptoManiaSwiftUI/Source/ContentView.swift
@@ -0,0 +1,18 @@
+//
+//  ContentView.swift
+//  CryptoManiaSwiftUI
+//
+//  Created by John Motta on 18/10/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        LoginView()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ CryptoManiaSwiftUI/Source/CryptoManiaSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  CryptoManiaSwiftUIApp.swift
+//  CryptoManiaSwiftUI
+//
+//  Created by John Motta on 18/10/24.
+//
+
+import SwiftUI
+
+@main
+struct CryptoManiaSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
