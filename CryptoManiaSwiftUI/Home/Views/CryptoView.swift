8abce7f3489303669b70d0ad70305e574331ecd6
John Motta <gabriel_motta31@hotmail.com>
2024-10-24 09:28:25 -0300

    Add crypto list with price and favorite toggle to CryptoView

--- CryptoManiaSwiftUI/Home/Views/CryptoView.swift
+++ CryptoManiaSwiftUI/Home/Views/CryptoView.swift
@@ -9,68 +9,71 @@ import SwiftUI
 
 struct CryptoView: View {
     @StateObject var viewModel = CryptoViewModel()
-    
+        
     var body: some View {
-        VStack {
-            List(Array(viewModel.crypto.enumerated()), id: \.element.id) { index, crypto in
-                HStack {
-                    AsyncImage(url: URL(string: "https://s2.coinmarketcap.com/static/img/coins/64x64/\(crypto.id).png")) { image in
-                        image
-                            .resizable()
-                            .scaledToFit()
-                        
-                    } placeholder: {
-                        ProgressView()
-                    }
-                    
-                    VStack(alignment: .leading) {
-                        HStack {
-                            Text(crypto.name)
-                            Text("|")
-                            Text(crypto.symbol)
+        NavigationStack {
+            VStack {
+                List(Array(viewModel.crypto.enumerated()), id: \.element.id) { index, crypto in
+                    HStack {
+                        AsyncImage(url: URL(string: "https://s2.coinmarketcap.com/static/img/coins/64x64/\(crypto.id).png")) { image in
+                            image
+                                .resizable()
+                                .scaledToFit()
                             
-                            Spacer()
-                            
-                            Text(String(format: "$%.2f", crypto.quote.usd.price))
+                        } placeholder: {
+                            ProgressView()
                         }
                         
-                        Spacer()
-                        
-                        HStack(spacing: 16) {
-                            Text(String(format: "%.2f%%", crypto.quote.usd.percentChange1h))
-                                .foregroundStyle(crypto.quote.usd.percentChange1h >= 0 ? .green : .red)
-                                .font(.caption).bold()
-                            
-                            Text(String(format: "%.2f%%", crypto.quote.usd.percentChange24h))
-                                .foregroundStyle(crypto.quote.usd.percentChange24h >= 0 ? .green : .red)
-                                .font(.caption).bold()
-                            
-                            
-                            Text(String(format: "%.2f%%", crypto.quote.usd.percentChange7d))
-                                .foregroundStyle(crypto.quote.usd.percentChange7d >= 0 ? .green : .red)
-                                .font(.caption).bold()
-                            
-                            Spacer()
-                            
-                            Button {
-                                if self.viewModel.crypto[index].favorite == nil {
-                                    self.viewModel.crypto[index].favorite = false
-                                }
-
-                                viewModel.crypto[index].favorite?.toggle()
-                            } label: {
-                                Image(systemName: crypto.favorite ?? false ? "heart.fill" :  "heart")
-                                    .foregroundStyle(.red)
-                                    .font(.title)
+                        VStack(alignment: .leading) {
+                            HStack {
+                                Text(crypto.name)
+                                Text("|")
+                                Text(crypto.symbol)
+                                
+                                Spacer()
+                                
+                                Text(String(format: "$%.2f", crypto.quote.usd.price))
                             }
-                            .buttonStyle(PlainButtonStyle())
+                            
+                            Spacer()
+                            
+                            HStack(spacing: 16) {
+                                Text(String(format: "%.2f%%", crypto.quote.usd.percentChange1h))
+                                    .foregroundStyle(crypto.quote.usd.percentChange1h >= 0 ? .green : .red)
+                                    .font(.caption).bold()
+                                
+                                Text(String(format: "%.2f%%", crypto.quote.usd.percentChange24h))
+                                    .foregroundStyle(crypto.quote.usd.percentChange24h >= 0 ? .green : .red)
+                                    .font(.caption).bold()
+                                
+                                
+                                Text(String(format: "%.2f%%", crypto.quote.usd.percentChange7d))
+                                    .foregroundStyle(crypto.quote.usd.percentChange7d >= 0 ? .green : .red)
+                                    .font(.caption).bold()
+                                
+                                Spacer()
+                                
+                                Button {
+                                    if self.viewModel.crypto[index].favorite == nil {
+                                        self.viewModel.crypto[index].favorite = false
+                                    }
+                                    
+                                    viewModel.crypto[index].favorite?.toggle()
+                                } label: {
+                                    Image(systemName: crypto.favorite ?? false ? "heart.fill" :  "heart")
+                                        .foregroundStyle(.red)
+                                        .font(.title2)
+                                }
+                                .buttonStyle(PlainButtonStyle())
+                            }
                         }
                     }
+                    .frame(height: 80)
                 }
-                .frame(height: 80)
+             
             }
+            .onAppear(perform: viewModel.fetchData)
         }
-        .onAppear(perform: viewModel.fetchData)
     }
 }
 
