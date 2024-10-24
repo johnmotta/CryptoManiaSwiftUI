5338f5aa18462bdbe22acd93fc4537d7c7b36518
John Motta <gabriel_motta31@hotmail.com>
2024-10-24 09:21:53 -0300

    Create TabBarView

---
+++ CryptoManiaSwiftUI/Home/Views/TabBarView.swift
@@ -0,0 +1,18 @@
+//
+//  TabBarView.swift
+//  CryptoManiaSwiftUI
+//
+//  Created by John Motta on 23/10/24.
+//
+
+import SwiftUI
+
+struct TabBarView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    TabBarView()
+}
